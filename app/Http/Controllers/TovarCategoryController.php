26703bdfed764016066e51df965341a2be02912b
vszeuszeus <stanis.homenko@gmail.com>
2017-08-15 00:32:31 +0600

    ended tovar category tovar podcategory contollers

--- app/Http/Controllers/TovarCategoryController.php
+++ app/Http/Controllers/TovarCategoryController.php
@@ -3,8 +3,83 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Tovar_category;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Validator;
+
 
 class TovarCategoryController extends Controller
 {
-    //
+    public function show()
+    {
+        return view('admin.tovar_category.show');
+    }
+
+    public function create()
+    {
+        return view('admin.tovar_category.create');
+    }
+
+    public function store(Request $request)
+    {
+
+        $this->validate($request, [
+            'name' => 'required|min:3|max:255|unique:tovar_categories,name',
+            'description' => 'max:3000',
+        ]);
+
+
+        $tovar = new Tovar_category();
+        $tovar->name = $request->tovar_name;
+        $tovar->name_eng = str_slug($request->tovar_name);
+        $tovar->description = $request->description;
+        $tovar->save();
+
+        return redirect()->back();
+
+    }
+
+    public function edit(Tovar_category $tovar_category)
+    {
+
+        return view('admin.tovar_category.edit', ['tovar_category' => $tovar_category]);
+    }
+
+    public function update(Request $request, Tovar_category $tovar_category)
+    {
+
+
+        $validator = Validator::make($request->all(), [
+            'name' => [
+                'required',
+                'min:3',
+                'max:255',
+                Rule::unique('tovars_categories')->ignore($tovar_category->id, 'id'),
+            ],
+            'description' => 'max:3000',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $tovar_category->name = $request->name;
+        $tovar_category->name_eng = str_slug($request->name);
+        $tovar_category->description = $request->description;
+        $tovar_category->save();
+        $tovar_category->save();
+
+
+        return redirect()->back();
+
+    }
+
+    public function destroy(Request $request, Tovar_category $tovar_category)
+    {
+        $tovar_category->delete();
+        $request->session()->flash('status', 'Товар успешно удален');
+        return redirect()->back();
+    }
 }
--- app/Http/Controllers/TovarPodcategoryController.php
+++ app/Http/Controllers/TovarPodcategoryController.php
@@ -3,8 +3,89 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Tovar_podcategory;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Validator;
 
 class TovarPodcategoryController extends Controller
 {
-    //
+
+    public function show()
+    {
+        return view('admin.tovar_podcategory.show', ['tovar_podcategories' => Tovar_podcategory::with('tovar_category')->get()]);
+    }
+
+    public function create()
+    {
+        return view('admin.tovar_podcategory.create');
+    }
+
+    public function store(Request $request)
+    {
+
+        $this->validate($request, [
+            'tovar_name' => 'required|min:3|max:255|unique:tovar_categories,name',
+            'description' => 'max:3000',
+            'tovar_category' => 'required|exists:tovar_categories,id'
+        ]);
+
+
+        $tovar = new Tovar();
+        $tovar->name = $request->tovar_name;
+        $tovar->name_eng = str_slug($request->tovar_name);
+        $tovar->description = $request->description;
+        $tovar->tovar_category_id = $request->tovar_category;
+        $tovar->save();
+
+        $request->session()->flash('status', 'Категория успешно добавлена');
+
+        return redirect()->back();
+
+    }
+
+    public function edit(Tovar_podcategory $tovar_podcategory)
+    {
+
+        return view('admin.tovar_podcategory.edit', ['tovar_podcategory' => $tovar_podcategory->load('tovar_category')]);
+    }
+
+    public function update(Request $request, Tovar_podcategory $tovar_podcategory)
+    {
+
+
+        $validator = Validator::make($request->all(), [
+            'name' => [
+                'required',
+                'min:3',
+                'max:255',
+                Rule::unique('tovar_podcategories')->ignore($tovar_podcategory->id, 'id'),
+            ],
+            'description' => 'max:3000',
+            'tovar_category' => 'required|exists:tovar_categories,id'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $tovar_podcategory->name = $request->name;
+        $tovar_podcategory->name_eng = str_slug($request->name);
+        $tovar_podcategory->description = $request->description;
+        $tovar_podcategory->tovar_category_id = $request->tovar_category;
+        $tovar_podcategory->save();
+
+        $request->session()->flash('status', 'Категория успешно изменена');
+
+        return redirect()->back();
+
+    }
+
+    public function destroy(Request $request, Tovar_podcategory $tovar_podcategory)
+    {
+        $tovar_podcategory->delete();
+        $request->session()->flash('status', 'Категория успешно удалена');
+        return redirect()->back();
+    }
 }
